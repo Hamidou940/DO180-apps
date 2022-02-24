2d21573597b8ab0511217f9f2b7e3fe030cdaf58
hamidou.mary <77271828+Hamidou940@users.noreply.github.com>
2022-02-24 09:49:52 +0100

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,4 @@
 <?php
-print "Welcome to the php website with openshift ! php version is " . PHP_VERSION . "\n";
+print "Welcome to the php website with openshift !
+J'ai voulu modifié la phrase pour tester !! php version is " . PHP_VERSION . "\n";
 ?>
